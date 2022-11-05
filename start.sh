0df9de6eede1c7f91e1fd0121bb61fed227d5745
thayeeboi890 <101670923+thayeeboi890@users.noreply.github.com>
2022-11-05 10:21:52 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 echo "starting, please wait..."
+tar -xv -f frames.tar.xz
+echo "finished extracting. please wait..."
 sleep 2
 mpv song.mp3 | ./main
 echo "HUGE THANKS TO leafyao8621 FOR PROVIDING THE FRAMEWORKS!"
+rm -R frames
 sleep 1
 echo "made by thayeeboi890"
